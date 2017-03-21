69b539b57731500f33db9c0250763df14ad94149
philbev <phil@philbev.co.uk>
2017-03-21 15:02:54 +0000

    ~/.emacs.d/init.el: auto-fill minor mode added to org-mode.
    File also tidied up.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,45 +47,25 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
-;;(package-initialize)
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;        org-mode configuration            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'org)
 (define-key org-mode-map (kbd "\C-c a") 'org-agenda)
-;(define-key org-mode-map (kbd "\C-c\C-j") 'org-journal-new-entry)
-;; <S-left> and <S-right> don't work in konsole so using 'Control' rather than 'Shift' modifier key.
 (define-key org-mode-map (kbd "<C-left>") 'org-shiftleft)
 (define-key org-mode-map (kbd "<C-right>") 'org-shiftright)
 (setq org-icalendar-include-todo t)
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (define-key global-map "\C-cc" 'org-capture)
-
-;; Evil configuration
-;;(setq evil-want-C-i-jump nil)
-;;(require 'evil)
-;;(evil-mode -1)
-
-(require 'powerline)
-(powerline-center-evil-theme)
-
-(require 'airline-themes)
-(load-theme 'airline-light)
-;; These lines are required for powerline and airline-themes. Glyphs do not display correctly otherwise.
-(setq powerline-utf-8-separator-left        #xe0b0
-      powerline-utf-8-separator-right       #xe0b2
-      airline-utf-glyph-separator-left      #xe0b0
-      airline-utf-glyph-separator-right     #xe0b2
-      airline-utf-glyph-subseparator-left   #xe0b1
-      airline-utf-glyph-subseparator-right  #xe0b3
-      airline-utf-glyph-branch              #xe0a0
-      airline-utf-glyph-readonly            #xe0a2
-      airline-utf-glyph-linenumber          #xe0a1)
-
+(add-hook 'org-mode-hook (auto-fill-mode t))
 (if (not (display-graphic-p))
   (ignore)
   (require 'org-bullets)
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;(setq org-journal-dir "/usr/local/docs/journal/")
 (require 'org-journal)
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/Dropbox/Apps/Emacs/tasks.org" "Tasks")
@@ -104,6 +84,26 @@
 	                  "* TODO \n  - %?\n  %i\n  %a")
 	("v" "Vim" entry (file+headline "~/Dropbox/Apps/Emacs/vim.org" "New Capture")
 	                  "* TODO \n  - %?\n  %i\n  %a")))
+
+
+(require 'powerline)
+(powerline-center-evil-theme)
+
+(require 'airline-themes)
+(load-theme 'airline-light)
+;; These lines are required for powerline and airline-themes. Glyphs do not display correctly otherwise.
+(setq powerline-utf-8-separator-left        #xe0b0
+      powerline-utf-8-separator-right       #xe0b2
+      airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
+
+
+
 
 ;; auto close bracket insertion. New in emacs 24
 (electric-pair-mode 1)
@@ -126,7 +126,10 @@
 ;;Highlight the cursor line.
 (global-hl-line-mode)
 
-;;Some key binding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               Some key binding                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (global-set-key (kbd "<f5>") 'flyspell-mode)
 (global-set-key (kbd "C-<f5>") 'linum-mode)
 (global-set-key (kbd "C-z") 'evil-mode)
@@ -137,3 +140,9 @@
 ;;Relative line numbers just like vim :-)
 (require 'linum-relative)
 (linum-relative-on)
+
+
+;; Evil configuration
+;;(setq evil-want-C-i-jump nil)
+;;(require 'evil)
+;;(evil-mode -1)
