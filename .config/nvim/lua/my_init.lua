0c271581a0a6de57dff401f8fb61ce6e67df3809
philbev <phil@philbev.co.uk>
2021-10-27 09:27:10 +0100

    Renaming of keymappings.lua to keymaps.lua.

--- .config/nvim/lua/my_init.lua
+++ .config/nvim/lua/my_init.lua
@@ -150,4 +150,4 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 
-require 'keyMappings'
+require 'keymaps'
