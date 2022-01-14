dc43b8b7e4c7e73dac390b964f6a5a6c7a121a51
philbev <phil@philbev.co.uk>
2022-01-14 09:57:13 +0000

    Configured bash language server.

--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -5,4 +5,8 @@
 -- See https://github.com/neovim/nvim-lspconfig for more details.
 -- Also https://github.com/williamboman/nvim-lsp-installer
 
+-- Python server
 require'lspconfig'.pyright.setup{}
+
+-- Bash server
+require'lspconfig'.bashls.setup{}
