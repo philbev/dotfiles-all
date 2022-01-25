2fb05f7c80a7df21aabd9f27f829dc5f28e3413b
philbev <phil@philbev.co.uk>
2022-01-25 10:28:58 +0000

    Nvim: Configuration deleted completely.

--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,7 +0,0 @@
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-map('n', '<F5>', ':w | terminal lua %<cr>a', opts)
-map('n', '<F22>', ':source ~/.config/nvim/ftplugin/lua.lua<cr>', opts)  -- <F22> = Shift + <F10>
-map('n', '<F10>', ':edit ~/.config/nvim/ftplugin/lua.lua<cr>', opts)
