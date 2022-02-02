dee37c868f48288db5fa6cd771d2fc0e5e14b17d
philbev <phil@philbev.co.uk>
2022-02-02 09:38:04 +0000

    Neovim: Lualine now displaying 'SPELl' and 'PASTE' options.

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -10,30 +10,32 @@ if not status_ok then
 end
 
 lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'dracula',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
+	options = {
+		icons_enabled = true,
+		theme = 'dracula',
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		},
+	sections = {
+		lualine_a = {'mode',
+		function() if vim.o.spell then return 'SPELL' else return '' end end,
+			function() if vim.o.paste then return 'PASTE' else return '' end end},
+		lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_c = {'filename'},
+		lualine_x = {'encoding', 'fileformat', 'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+		},
+	tabline = {},
+	extensions = {}
 }
