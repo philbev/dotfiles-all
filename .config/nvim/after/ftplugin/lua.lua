334b31fa36cafd610adab1fde90337dc0fdabdfe
philbev <phil@philbev.co.uk>
2022-02-08 09:48:50 +0000

    Neovim: lua ftplugin <F5> mapping.

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -11,5 +11,6 @@ local opts = { noremap = true, silent = true }
 
 map('n', '<F10>', ':edit ~/.config/nvim/after/ftplugin/lua.lua<cr>', opts)
 map('n', '<F22>', ':source ~/.config/nvim/after/ftplugin/lua.lua<cr>', opts)
+map('n', '<F5>', ':w | !lua %<cr>', opts)
 
 -- vim.api.nvim_win_set_option(0, 'spell', true)
