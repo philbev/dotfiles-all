d039564f0aee471365c82666350524a34b50f2ca
philbev <phil@philbev.co.uk>
2022-03-01 15:46:10 +0000

    Ftplugin help.lua added to repo.

---
+++ .config/nvim/after/ftplugin/help.lua
@@ -0,0 +1,17 @@
+--  _          _        _
+-- | |__   ___| |_ __  | |_   _  __ _
+-- | '_ \ / _ \ | '_ \ | | | | |/ _` |
+-- | | | |  __/ | |_) || | |_| | (_| |
+-- |_| |_|\___|_| .__(_)_|\__,_|\__,_|
+--              |_|
+
+
+local map = vim.api.nvim_buf_set_keymap
+local opts = { noremap = true, silent = true }
+
+map(0, 'n', '<F10>', ':edit ~/.config/nvim/after/ftplugin/help.lua<cr>', opts)
+map(0, 'n', '<F22>', ':source ~/.config/nvim/after/ftplugin/help.lua<cr>', opts)
+map(0, 'n', '<space>', '', opts)
+map(0, 'n', 'b', '', opts)
+
+-- vim.api.nvim_win_set_option(0, 'spell', true)
