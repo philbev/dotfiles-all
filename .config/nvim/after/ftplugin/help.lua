2731feffb49471a165db02626237881e1ae89213
philbev <phil@philbev.co.uk>
2022-03-02 10:33:05 +0000

    help.lua: ftplugin - new key mappings.

--- .config/nvim/after/ftplugin/help.lua
+++ .config/nvim/after/ftplugin/help.lua
@@ -12,6 +12,9 @@ local opts = { noremap = true, silent = true }
 map(0, 'n', '<F10>', ':edit ~/.config/nvim/after/ftplugin/help.lua<cr>', opts)
 map(0, 'n', '<F22>', ':source ~/.config/nvim/after/ftplugin/help.lua<cr>', opts)
 map(0, 'n', '<space>', '', opts)
+map(0, 'n', 'x', '', opts)
 map(0, 'n', 'b', '', opts)
+map(0, 'n', '<CR>', '', opts)
+map(0, 'n', '<Del>', '/|[a-zA-Z:]<cr>', opts) -- <Del> key searches for next hyperlink.
 
 -- vim.api.nvim_win_set_option(0, 'spell', true)
