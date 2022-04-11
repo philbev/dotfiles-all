55ff79e40bce180cb152233b52e64c0c008eecf8
philbev <phil@philbev.co.uk>
2022-04-11 09:13:29 +0100

    Neovim: Treesitter configuration.

--- .config/nvim/lua/setup/treesitter.lua
+++ .config/nvim/lua/setup/treesitter.lua
@@ -13,7 +13,7 @@ end
 -- require'nvim-treesitter.configs'.setup {
 configs.setup {
 	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = "maintained",
+	ensure_installed = "all",
 
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
