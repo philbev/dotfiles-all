b8b234f1dc30fc6ab3478f2f7186eb8143d403be
philbev <phil@philbev.co.uk>
2022-04-11 09:11:06 +0100

    neovim: New key maps.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,30 @@
---  ___       _ _       _
--- |_ _|_ __ (_) |_    | |_   _  __ _
---  | || '_ \| | __|   | | | | |/ _` |
---  | || | | | | |_  _ | | |_| | (_| |
--- |___|_| |_|_|\__|(_)|_|\__,_|\__,_|
+--  _       _ _     _
+-- (_)_ __ (_) |_  | |_   _  __ _
+-- | | '_ \| | __| | | | | |/ _` |
+-- | | | | | | |_ _| | |_| | (_| |
+-- |_|_| |_|_|\__(_)_|\__,_|\__,_|
+--
 
 -- ~/.config/nvim/init.lua
 
-require('setup.settings')
-require('setup.mappings')
-require('setup.packer')
-require('setup.ultisnips')
-require('setup.lualine')
-require('setup.barbar')
-require('setup.telescope')
-require('setup.whichkey')
-require('setup.completion')
-require('setup.autopairs')
-require('setup.indent-blankline')
-require('setup.nvim-tree')
-require('setup.nvim-colorizer')
-require('setup.lspconfig')
-require('setup.lsp-installer')
-require('setup.gitsigns')
-require('setup.treesitter')
-require('setup.notify')
-require('setup.dracula')
-require('setup.luasnip')
--- require('setup.teej-luasnip')
+require 'setup.settings'
+require 'setup.mappings'
+require 'setup.packer'
+require 'setup.ultisnips'
+require 'setup.lualine'
+require 'setup.barbar'
+require 'setup.telescope'
+require 'setup.whichkey'
+require 'setup.completion'
+require 'setup.autopairs'
+require 'setup.indent-blankline'
+require 'setup.nvim-tree'
+require 'setup.nvim-colorizer'
+require 'setup.lspconfig'
+require 'setup.lsp-installer'
+require 'setup.gitsigns'
+require 'setup.treesitter'
+require 'setup.notify'
+require 'setup.dracula'
+require 'setup.luasnip'
+-- require 'setup.teej-luasnip'
--- .config/nvim/lua/setup/mappings.lua
+++ .config/nvim/lua/setup/mappings.lua
@@ -19,33 +19,38 @@
 -- vim.api.nvim_buf_del_keymap()
 
 
-local map = vim.api.nvim_set_keymap
+-- local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- NORMAL MODE MAPPINGS
-map('n', '<leader>x', ':xa<cr>', opts)
-map('n', '<leader>s', ':w | so %<cr>', opts)
-map('n', '<leader>q', ':qa<cr>', opts)
-map('n', '<leader>w', ':wa<cr>', opts)
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+map('n', '<F12>', ':edit $MYVIMRC<cr>', opts)
+map('n', '<F24>', ':source $MYVIMRC<cr>', opts)		-- <F24> => <S-F12>
 map('n', '<F2>', ':nohl<cr>', opts)
 map('n', '<F3>', ':set spell! spell?<cr>', opts)
 map('n', '<F4>', ':set relativenumber! relativenumber?<cr>', opts)
-map('n', '<F12>', ':edit $MYVIMRC<cr>', opts)
-map('n', '<F24>', ':source $MYVIMRC<cr>', opts)		-- <F24> = <S-F12>
+map('n', '<M-,>', ':bprevious<cr>', opts)
+map('n', '<M-.>', ':bnext<cr>', opts)
 map('n', '<M-Del>', ':bdelete<cr>', opts)
 map('n', '<M-Down>', ':m +1<cr>', opts)
 map('n', '<M-Up>', ':m -2<cr>', opts)
-map('n', '<M-.>', ':bnext<cr>', opts)
-map('n', '<M-,>', ':bprevious<cr>', opts)
-map('n', '<M-n>', '<C-w>w', opts)
 map('n', '<M-c>', '<C-w>c', opts)
 map('n', '<M-l>', ':set list! list?<cr>', opts)
+map('n', '<M-n>', '<C-w>w', opts)
 map('n', '<M-o>', ':only<cr>', opts)
 map('n', '<leader>gs', ':Git<cr>', opts)
-vim.keymap.set({'n'}, "<M-w>", '<cmd>set wrap! wrap?<cr>', opts)
-vim.keymap.set({'n'}, '<M-d>', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>')
-vim.keymap.set({'n'}, '<M-u>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>')
-vim.keymap.set({'n'}, '<M-/>', '<cmd>Neotree toggle<cr>')
+map('n', '<leader>q', ':qa<cr>', opts)
+map('n', '<leader>s', ':w | so %<cr>', opts)
+map('n', '<leader>w', ':wa<cr>', opts)
+map('n', '<leader>x', ':xa<cr>', opts)
+map({'n'}, "<M-w>", '<cmd>set wrap! wrap?<cr>', opts)
+map({'n'}, '<M-/>', '<cmd>Neotree toggle<cr>')
+map({'n'}, '<M-d>', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>')
+map({'n'}, '<M-u>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>')
 
 -- VISUAL MODE MAPPINGS
 map('v', '<M-Down>', ":m '>+1<cr>gv", opts)
